04b94b92814edbb683abe4642bae9aee183fbc2b
msk-fjnzfdn <romanshalyatov@yandex.ru>
2026-01-25 14:43:18 +0300

    Changed map

--- assets/maps/chest.tsx
+++ assets/maps/chest.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="chest" tilewidth="45" tileheight="45" tilecount="484" columns="22">
- <image source="chest.png" width="1024" height="1024"/>
+<tileset version="1.10" tiledversion="1.11.2" name="chest" tilewidth="45" tileheight="45" tilecount="25" columns="5">
+ <image source="chest.png" width="256" height="256"/>
 </tileset>
